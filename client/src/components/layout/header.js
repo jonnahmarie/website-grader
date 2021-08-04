64802c410bb7d9f9e823ca4913ca08c73aaa6580
jonnahmarie <jonnah.marie@gmail.com>
2021-08-04 12:09:29 -0700

    removing useeffect for logo sizing

--- client/src/components/layout/header.js
+++ client/src/components/layout/header.js
@@ -1,28 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import logoFM from '../../images/FerociousMEDIALogo_H-01.png';
 
 const Header = () => {
-    const desktop = '30%';
-    const mobile = '70%'
-
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const logoSize = windowWidth >=650 ? desktop : mobile;
-
-    useEffect(() => {
-        const handleWindowResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleWindowResize);
-
-        return () => {
-            window.removeEventListener('resize', handleWindowResize);
-        }
-    }, []);
-
     return (
         <div className='secondary-bar'>
-            <img src={logoFM} alt='Ferocious Media' style={{width:`${logoSize}`, float:'right'}}/>
+            <img src={logoFM} alt='Ferocious Media' style={{width:'30%', float:'right'}}/>
         </div>
     )
 }
